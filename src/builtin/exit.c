e2ed50ff992c5454cf11a41c367b85ee9bb0933d
Noah <noah.gosciniak@epitech.eu>
2022-05-24 16:54:23 +0200

    FIX: printing exit when is_prompt_mode is enable

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -22,12 +22,17 @@ int builtin_exit(sh_data_t *data)
         return (1);
     }
     if (data->last_exit_status != 0) {
-        my_printf("exit\n");
+        if (data->is_prompt_mode)
+            my_printf("exit\n");
         exit(data->last_exit_status);
     }
-    if (data->current_command->argc == 1)
-        default_shell_exit();
-    my_printf("exit\n");
+    if (data->current_command->argc == 1) {
+        if (data->is_prompt_mode)
+            my_printf("exit\n");
+        exit(0);
+    }
+    if (data->is_prompt_mode)
+        my_printf("exit\n");
     int exit_code = my_getnbr(data->current_command->argv[1]);
     exit(exit_code);
     return (0);
