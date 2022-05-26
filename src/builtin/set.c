15c9975da329e324d18b4a3a48ed441a165221c6
Noah <noah.gosciniak@epitech.eu>
2022-05-26 10:50:19 +0200

    FIX: handling spaces in set builtin, (first version)

--- src/builtin/set.c
+++ src/builtin/set.c
@@ -8,6 +8,37 @@
 #include "shell.h"
 #include "my.h"
 
+void extend_array(char ***array, char *new_line);
+
+char **handling_spaces(sh_data_t *data)
+{
+    char **array = malloc(sizeof(char *));
+    array[0] = NULL;
+    char *cmd;
+    int len;
+
+    for (int i = 0; data->current_command->argv[i] != NULL; i++) {
+        if (data->current_command->argv[i + 1] != NULL &&
+        data->current_command->argv[i + 2] != NULL) {
+            if (my_strlen(data->current_command->argv[i + 1]) == 1 &&
+            data->current_command->argv[i + 1][0] == '=') {
+                len = my_strlen(data->current_command->argv[i]) + my_strlen(data->current_command->argv[i + 1]) + my_strlen(data->current_command->argv[i + 2]);
+                cmd = malloc(sizeof(char) * (len + 1));
+                for (int j = 0; j < len; cmd[j] = 0, j++);
+                cmd = my_strcat(cmd, data->current_command->argv[i]);
+                cmd = my_strcat(cmd, data->current_command->argv[i + 1]);
+                cmd = my_strcat(cmd, data->current_command->argv[i + 2]);
+                i += 2;
+            } else
+                cmd = my_strdup(data->current_command->argv[i]);
+        } else
+            cmd = my_strdup(data->current_command->argv[i]);
+        extend_array(&array, my_strdup(cmd));
+        free(cmd);
+    }
+    return (array);
+}
+
 void set_var_value(sh_data_t *data, char *name, char *value)
 {
     int pos = get_var_line_pos(data, name);
@@ -39,12 +70,6 @@ with a letter.\n") : 0;
             error = 1;
             data->current_command->argv[i] = "";
         }
-        if (my_strstr(str, "\t") != NULL) {
-            error == 0 ? my_fprintf(2, "set: Variable name must contain \
-alphanumeric characters.\n") : 0;
-            error = 1;
-            data->current_command->argv[i] = "";
-        }
     }
     return (0);
 }
@@ -58,6 +83,7 @@ int builtin_set(sh_data_t *data)
             my_printf("%s\n", data->vars[i]);
         return (0);
     }
+    data->current_command->argv = handling_spaces(data);
     int check = check_set(data);
     if (check)
         return (check);
@@ -68,6 +94,5 @@ int builtin_set(sh_data_t *data)
         } else
             set_var_value(data, data->current_command->argv[i], "");
     }
-
     return (0);
 }
