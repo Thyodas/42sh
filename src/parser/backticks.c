1d123932f7d8036e30f5ab7e1e523ca8f622ab8f
Noah <noah.gosciniak@epitech.eu>
2022-05-29 14:21:07 +0200

    FIX: valgrind

--- src/parser/backticks.c
+++ src/parser/backticks.c
@@ -13,13 +13,7 @@
 char *clean_str(char *str);
 char *reformat(char *str);
 char **dup_array(char **array);
-
-static void close_dup(int a, int b)
-{
-    close(b);
-    dup2(a, b);
-    close(a);
-}
+void close_dup(int a, int b);
 
 static char *sheitan_part(sh_data_t *data, char *cmd, int old_out, int fd[2])
 {
@@ -27,8 +21,6 @@ static char *sheitan_part(sh_data_t *data, char *cmd, int old_out, int fd[2])
     char buffer[message_size];
     ssize_t size;
 
-    cmd[my_strlen(cmd) - 1] = '\0';
-    cmd = &cmd[1];
     close_dup(fd[1], STDOUT_FILENO);
     parse_current_line(data, cmd);
     close(fd[1]);
@@ -56,21 +48,50 @@ char *get_backticks_value(sh_data_t *data, char *cmd)
     return (out);
 }
 
-int handle_backtick(sh_data_t *data)
+static char *getbline(sh_data_t *data, char *cmd, char *first, char *second)
 {
     char *str;
+    int len;
+    char *res;
+
+    str = get_backticks_value(data, my_strdup(cmd));
+    str = reformat(str);
+    len = my_strlen(str) + my_strlen(first) + my_strlen(second);
+    res = malloc(sizeof(char) * (len + 1));
+    for (int j = 0; j <= len; res[j] = 0, j++);
+    res = my_strcat(my_strcat(my_strcat(res, first), str), second);
+    return (my_strdup(res));
+}
+
+static int find_backticks(sh_data_t *data, char **oldlines, int i)
+{
+    char *cmd;
+    char *cmd_end;
+    char *first;
+    char *second;
+
+    first = my_strdup(oldlines[i]);
+    if ((cmd = my_strstr(first, "`"))) {
+        cmd[0] = '\0';
+        cmd = my_strdup(&cmd[1]);
+        if ((cmd_end = my_strstr(cmd, "`"))) {
+            second = my_strlen(cmd_end) > 1 ? &cmd_end[1] : my_strdup("");
+            cmd_end[0] = '\0';
+        }
+        free(oldlines[i]);
+        oldlines[i] = getbline(data, cmd, first, second);
+        return (data->last_exit_status);
+    }
+    return (0);
+}
+
+int handle_backtick(sh_data_t *data)
+{
     char **oldlines = dup_array(data->line);
     int exit_status = 0;
 
-    for (int i = 0; oldlines[i]; i++) {
-        if (oldlines[i][0] == '`') {
-            str = get_backticks_value(data, my_strdup(oldlines[i]));
-            free(oldlines[i]);
-            str = reformat(str);
-            oldlines[i] = my_strdup(str);
-            exit_status = data->last_exit_status;
-        }
-    }
+    for (int i = 0; oldlines[i]; i++)
+        exit_status = find_backticks(data, oldlines, i);
     data->line = oldlines;
     return (exit_status);
 }
